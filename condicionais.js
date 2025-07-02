30b4875e7732a13724e30afe9a0e80194b7ad509
F3190196 Felipe Marques carvalho dos Santos <felipemarques@bb.com.br>
2025-07-02 14:06:08 -0300

    Exercício Switch Case

--- condicionais.js
+++ condicionais.js
@@ -1,4 +1,4 @@
-//Exercício Condicionais
+//Exercício 1 Condicionais
 /*
 let num1 = parseInt(prompt("Digite um número:"));
 let num2 = parseInt(prompt("Digite outro número:"));
@@ -24,7 +24,7 @@ if(operacao === "+"){
 // Verificar se estudante receberá bônus na nota.
 // Estudantes recebem bônus se nota for 8 ou acima e se tiverem no máximo 2 faltas.
 // Usar operador ternário
-
+/*
 let nome = prompt("Digite o nome do aluno: ");
 let nota = parseInt(prompt("Digite a nota do aluno - Entre 0 e 10: "));
 while(nota<0 || nota>10){
@@ -33,3 +33,39 @@ while(nota<0 || nota>10){
 let faltas = parseInt(prompt("Digite a quantidade de faltas que esse aluno teve: "));
 let recebeBonus = nota>=8 && faltas<=2?`${nome} Receberá bônus na sua nota.`:`${nome} Não receberá o bonus na nota.`
 alert(recebeBonus);
+*/
+
+//Exercício 3
+// Switch Case
+
+let dia = parseInt(prompt("Digite qual é o dia da semana: \n0-Domingo\n1-Segunda\n2-Terça\n3-Quarta\n4-Quinta\n5-Sexta\n6-Sábado"));
+let listaDias = [0,1,2,3,4,5,6];
+let diaSemana=["Domingo","Segunda","Terça","Quarta","Quinta","Sexta","Sábado"];
+
+while(!listaDias.includes(dia)){
+    dia = parseInt(prompt("Digite qual é o dia da semana correto: \n0-Domingo\n1-Segunda\n2-Terça\n3-Quarta\n4-Quinta\n5-Sexta\n6-Sábado"));
+}
+
+switch(dia){
+    case 0:
+        alert(`Hoje é ${diaSemana[dia]}\nO Horário de funcionamento é de 10h às 14h`);
+        break;
+    case 1:
+        alert(`Hoje é ${diaSemana[dia]}\nO Horário de funcionamento é de 9h às 18h`);
+        break;
+    case 2:
+        alert(`Hoje é ${diaSemana[dia]}\nO Horário de funcionamento é de 9h às 18h`);
+        break;
+    case 3:
+        alert(`Hoje é ${diaSemana[dia]}\nO Horário de funcionamento é de 9h às 18h`);
+        break;
+    case 4:
+        alert(`Hoje é ${diaSemana[dia]}\nO Horário de funcionamento é de 9h às 18h`);
+        break;
+    case 5:
+        alert(`Hoje é ${diaSemana[dia]}\nO Horário de funcionamento é de 9h às 18h`);
+        break;
+    case 6:
+        alert(`Hoje é ${diaSemana[dia]}\nO Horário de funcionamento é de 10h às 14h`);
+        break;
+}
