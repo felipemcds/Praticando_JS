7a314431c5d9faa79f312fe78253922783af1dbb
F3190196 Felipe Marques carvalho dos Santos <felipemarques@bb.com.br>
2025-07-04 13:58:55 -0300

    Exercicio 3 FOR

--- lacosrep.js
+++ lacosrep.js
@@ -3,7 +3,7 @@
 //Interromper o laço caso o numero gerado seja 15
 //interromper depois de 30 tentativas e contar a quantidade de tentativas
 //usar Math.floor(Math.Random()*(n)+1)
-
+/*
 for(let i=1;i<=30;i++){
     let numeroAleatorio = Math.floor(Math.random()*(50)+1);
     if(numeroAleatorio===15){
@@ -15,3 +15,29 @@ for(let i=1;i<=30;i++){
         continue;
     }
 }
+*/
+//Exercício 2 FOR
+// Crie um programa que exiba todos os números pares de 1 até o número final fornecido pelo usuário.
+/*
+let numero = parseInt(prompt("Digite um numero: "));
+let listaPares =[];
+
+for (let i=1;i<=numero;i++){
+    if(i%2===0){
+        listaPares.push(i);
+    }
+}
+alert(`Esses são os números pares da sequência 1 à ${numero}: ${listaPares}`);
+console.log(listaPares);
+*/
+
+//Exercício 3 FOR 
+//Como você pode percorrer e exibir todos os caracteres de uma senha digitada, um por um?
+/*
+let senha = prompt("Digite sua senha: ");
+
+for(let i =0;i<senha.length;i++){
+    alert(`Caractere ${i+1}: ${senha[i]}`);
+}
+*/
+
