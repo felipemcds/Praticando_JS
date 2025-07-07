eed53fc0b30c9050f2154f9c7b239ca4ed5b5544
F3190196 Felipe Marques carvalho dos Santos <felipemarques@bb.com.br>
2025-07-07 15:12:42 -0300

    Exercicio 3 funções - Arrow functions

--- funcoes.js
+++ funcoes.js
@@ -16,7 +16,7 @@ alert(`O resultado de ${base} elevado ao ${expoente} é: ${resultado}`);
 // criar função que calcula o fatorial de um número usando recursão.
 // fatorial: n! multiplicação de n por seus antecessores maiores ou iguais a 1
 // ex: 5! = 5 x 4 x 3 x 2 x 1
-
+/*
 let numero = parseInt(prompt("Digite um número para descobrir o seu fatorial:"));
 
 const fatorial = function f(num){
@@ -25,3 +25,19 @@ const fatorial = function f(num){
 }
 
 alert(`O fatorial do número ${numero} é: ${fatorial(numero)}`)
+*/
+
+//Exercício 3 Arrow Functions
+// Criar uma funcao para calcular juros compostos
+// Funcao deve receber os valores no formato inteiro: valor, % de juros e Tempo
+// formula: valor*(juros elevador ao tempo)
+
+let valor = parseInt(prompt("Informe um valor para ser calculado o rendimento:"));
+let juros = parseInt(prompt("Informe a taxa de juros a.m:"));
+let meses = parseInt(prompt("Informe a quantidade de meses desejada:"));
+
+const valorFinal = (valorInicial,juros,tempo) => (valorInicial*((1+(juros/100))**tempo)); // Não uso function, uso os parâmetros que irei precisar
+ //como a minha arrow function só possui 1 linha, não preciso usar o return
+alert(`O rendimento de ${valor} a uma taxa de juros de ${juros}a.m em ${meses} meses é de: ${valorFinal(valor,juros,meses).toFixed(2)}`);
+
+
